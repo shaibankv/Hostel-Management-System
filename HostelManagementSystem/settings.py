290bd7f057605ace164b69983bd1f1c7c66190dc
MOHAMMED SHAIBAN <mohammedshaibanbinshihab786@gmail.com>
2026-06-07 17:55:23 +0530

    Update settings.py

--- HostelManagementSystem/settings.py
+++ HostelManagementSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-81r%*(-6jojy=4&l&dk!!q+vxsbm)$3&(#td*loq@q112ycpt(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
